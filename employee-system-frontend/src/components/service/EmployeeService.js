ccc17c2351b93ac348b1c6bec37da9bdc0b53c37
Anthony4m <amoahanthony41@gmail.com>
2022-08-10 20:45:22 +0000

    changed baseurl

--- employee-system-frontend/src/components/service/EmployeeService.js
+++ employee-system-frontend/src/components/service/EmployeeService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Employees from "../Employees";
 
-const EMPLOYEE_BASE_URL = "http://localhost:8080/api/v1/employees";
+const EMPLOYEE_BASE_URL = "http://localhost:8087/api/v1/employees";
 
 class EmployeeService{
 
